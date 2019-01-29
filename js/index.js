b10557d5882c42bad8d899870bca20d188881ec4
Daniel <danielstarling4@gmail.com>
2019-01-29 13:40:31 -0700

    add nav highlighting on mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// Nav Hover 
+
+
+// Color
+
+const navAnimate = document.querySelector("nav");
+
+navAnimate.addEventListener("mouseenter", function (event) {
+
+    // highlight the mouseenter target
+    event.target.style.color = "#17A2B8";
+
+    // reset the color after a short delay
+    setTimeout(function () {
+        event.target.style.color = "#212529";
+    }, 500);
+}, false);
+
+// this handler will be executed every time the cursor is moved over a different list item
+navAnimate.addEventListener("mouseover", function (event) {
+    // highlight the mouseover target
+    event.target.style.color = "#17A2B8";
+
+    // reset the color after a short delay
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 500);
+}, false);
