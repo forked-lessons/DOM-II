9c288e5861983dfef9b5331fc558b04ae496a7b9
Daniel <danielstarling4@gmail.com>
2019-01-29 13:53:48 -0700

    added size mouseover effect to the nav

--- js/index.js
+++ js/index.js
@@ -2,23 +2,10 @@
 
 // Nav Hover 
 
+const navAnimate = document.querySelector("nav");
 
 // Color
 
-const navAnimate = document.querySelector("nav");
-
-navAnimate.addEventListener("mouseenter", function (event) {
-
-    // highlight the mouseenter target
-    event.target.style.color = "#17A2B8";
-
-    // reset the color after a short delay
-    setTimeout(function () {
-        event.target.style.color = "#212529";
-    }, 500);
-}, false);
-
-// this handler will be executed every time the cursor is moved over a different list item
 navAnimate.addEventListener("mouseover", function (event) {
     // highlight the mouseover target
     event.target.style.color = "#17A2B8";
@@ -28,3 +15,16 @@ navAnimate.addEventListener("mouseover", function (event) {
         event.target.style.color = "";
     }, 500);
 }, false);
+
+// Size 
+
+navAnimate.addEventListener("mouseover", function (event) {
+
+    // highlight the mouseenter target
+    event.target.style["font-size"] = "30px";
+    // reset the color after a short delay
+    setTimeout(function () {
+        event.target.style["font-size"] = "20px";
+    }, 500);
+}, false);
+
